7902b903c9e3214c491d3649b547b0c558a0dd8a
Shawn Littrel <shawnlittrel@protonmail.com>
2021-04-20 19:27:26 -0700

    set up directory

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+//import Sequelize from the library
+const Sequelize = require('sequelize');
+
+//Create connection to database
+const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PW', {
+     host: 'localhost',
+     dialect: 'mysql',
+     port: 3306
+});
+
+module.exports = sequelize;
